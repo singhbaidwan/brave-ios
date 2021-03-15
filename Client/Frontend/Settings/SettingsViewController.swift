18f42a2aaedea42010a33bb83986edb5cb16fde5
Soner Yuksel <grkmyksl@gmail.com>
2021-03-15 15:29:47 -0400

    Adding Activity Manager and New Tab NSActivity

--- Client/Frontend/Settings/SettingsViewController.swift
+++ Client/Frontend/Settings/SettingsViewController.swift
@@ -249,6 +249,12 @@ class SettingsViewController: TableViewController {
             }, cellClass: MultilineButtonCell.self))
         }
         
+        general.rows.append(
+            .boolRow(title: Strings.alwaysRequestDesktopSite,
+                     option: Preferences.General.alwaysRequestDesktopSite,
+                     image: #imageLiteral(resourceName: "settings-desktop-always").template)
+        )
+        
         return general
     }()
     
